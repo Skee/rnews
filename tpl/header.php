8d2f52f41aa07fe3d160a45a2fd1a33c26652be8
wooptoo <wooptoo@gmail.com>
2009-02-16 03:26:33 +0200

    XHTML compliance

--- tpl/header.php
+++ tpl/header.php
@@ -1,8 +1,11 @@
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ro" lang="ro">
 <head>
-<title>omghy2u!</title>
+	<title>revolushii.ro | the newsblog libre</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="generator" content="RNews <?php echo $version; ?>" />
 </head>
 
 <body>
-<strong>HURRRR revo.ro frontpage!</strong>
-<br>
+
+<h1>revolushii.ro | the newsblog libre</h1>
