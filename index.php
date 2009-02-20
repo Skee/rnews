adc383686aa642c793cb383f42b5362cc27b397f
Mircea Suciu <mircea@gmail.com>
2009-02-20 14:11:03 +0200

    use first attachment as left-aligned image; retab file

--- index.php
+++ index.php
@@ -14,8 +14,8 @@ $version = "0.5";
 $page = $_GET['p'] + 0;
 if (!is_numeric($page) || $page < 0 || $page > 999)
 {
-	header("Location: /");
-	die();
+    header("Location: /");
+    die();
 }
 $items_start = ($page * $items_per_page);
 
@@ -23,8 +23,8 @@ $items_start = ($page * $items_per_page);
 // posts in that thread and returns that too. Sorted by time desc (newest
 // first), uninfluenced by replies.
 $n_query = "select *,count(id_msg)-1 AS num_comments from smf_messages where
-	id_board = $board_id group by id_topic order by -posterTime limit
-	$items_start, $items_per_page";
+    id_board = $board_id group by id_topic order by -posterTime limit
+    $items_start, $items_per_page";
 
 // For Subs.php bbc parsing:
 define("SMF", "muffins");
@@ -46,24 +46,38 @@ include($template_header);
 // parse database content, item by item
 while($item = mysql_fetch_assoc($raw))
 {
-	// do html magic hyah
-	echo '<div class="post">';
-	echo '<h2 class="storytitle">
-		<a rel="bookmark" href="' . $forum_url . '/index.php/topic,' .
-		$item['ID_TOPIC'] . '.0.html">' . $item['subject'] . '</a>
-		</h2>';
+    // get first attachment (second attachment is thumbnail)
+    $attach_query = "select id_attach from smf_attachments where id_msg = " .
+        $item['ID_MSG'] . " limit 1";
+    $att_res = mysql_query($attach_query) or die(mysql_error());
+    $attach_id = 0;
+    if(mysql_num_rows($att_res) > 0)
+    {
+        $attach_arr = mysql_fetch_row($att_res);
+        $attach_id = $attach_arr[0];
+    }
+    // do html magic hyah
+    echo '<div class="post">';
+    echo '<h2 class="storytitle">
+        <a rel="bookmark" href="' . $forum_url . '/index.php/topic,' .
+        $item['ID_TOPIC'] . '.0.html">' . $item['subject'] . '</a>
+        </h2>';
     echo '<p class="meta">Posted by <em> <a href="' . $forum_url .
         '/index.php?action=profile;u=' . $item['ID_MEMBER'] . '">' .
         $item['posterName'] . '</a></em> @ ' . date('H:i, D, d M Y',
         $item['posterTime']) . "</p>\n";
-	echo '<div class="storycontent">';
-		echo parse_bbc($item['body']) . '<br />';
-	echo "</div>\n";
-	echo '<p class="feedback">
-		<a href="' . $forum_url . '/index.php/topic,' .
-		$item['ID_TOPIC'] . '.0.html">' . $item['num_comments'] . ' comments</a>
-		</p>';
-	echo '</div>';
+    echo '<div class="storycontent">';
+    // if any attachment was found, display it left-aligned
+    if($attach_id) echo '<img src="forum/index.php?action=dlattach;topic=' .
+        $item['ID_TOPIC'] . '.0;attach=' . $attach_id . ';image" 
+        class="alignnone Lb" />';
+    echo parse_bbc($item['body']) . '<br />';
+    echo "</div>\n";
+    echo '<p class="feedback">
+        <a href="' . $forum_url . '/index.php/topic,' .
+        $item['ID_TOPIC'] . '.0.html">' . $item['num_comments'] . ' comments</a>
+        </p>';
+    echo '</div>';
 }
 
 // paging
@@ -86,20 +100,20 @@ echo "<h2>Latest game releases</h2>\n";
 echo "<p>\n";
 
 $nr_query = "select ID_MSG, subject as title from smf_messages where ID_TOPIC =
-	$thread_id and ID_MEMBER = $poster_id and left(subject, 3) != 'Re:' order
-	by -posterTime LIMIT 20";
+    $thread_id and ID_MEMBER = $poster_id and left(subject, 3) != 'Re:' order
+    by -posterTime LIMIT 20";
 
 $nr_raw = mysql_query($nr_query, $link);
 
 while($item = mysql_fetch_assoc($nr_raw))
 {
-	// output line by line and link to the forum post
-	// strip BBCode from title
-	$title = preg_replace("/(\\[.*?\\])/", "", $item['title']);
+    // output line by line and link to the forum post
+    // strip BBCode from title
+    $title = preg_replace("/(\\[.*?\\])/", "", $item['title']);
 
-	echo '<a href="' . $forum_url . '/index.php/topic,360.msg' .
-		$item['ID_MSG'] .  '.html#msg' . $item['ID_MSG'] . '">' . $title .
-		"</a><br />\n";
+    echo '<a href="' . $forum_url . '/index.php/topic,360.msg' .
+        $item['ID_MSG'] .  '.html#msg' . $item['ID_MSG'] . '">' . $title .
+        "</a><br />\n";
 }
 echo "</p>\n";
 echo '</div>';
@@ -109,3 +123,4 @@ echo '<!-- end sidebar -->';
 include($template_footer);
 
 ?>
+
